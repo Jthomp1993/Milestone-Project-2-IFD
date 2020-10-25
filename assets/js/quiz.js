3c088dbb81b32ece5c7c7735c71bb04af6456409
Jthomp1993 <joshuagt93@hotmail.com>
2020-10-25 12:44:48 +0000

    Added javascript to change quiz answers background color depending if true or false

---
+++ assets/js/quiz.js
@@ -0,0 +1,25 @@
+var results = 0;
+
+$(document).ready(function() {
+ 
+    $('.answers-btn').click(function() {
+        if($(this).hasClass('true')) {
+            $(this).addClass('btn-correct');
+        }
+        else { $(this).addClass('btn-incorrect');
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
