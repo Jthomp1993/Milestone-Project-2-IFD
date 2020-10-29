4a1e58d503ccbc972c0bbd53e5e630df18068228
Jthomp1993 <joshuagt93@hotmail.com>
2020-10-29 15:11:46 +0000

    Rendered the google maps api in live.html

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,5 +1,37 @@
-var locations = [
-{lat: 53.4742028, lng: -2.2447486},
-{lat: 53.809866, lng: -1.5729377},
-{ lat: 51.5235754, lng: -0.0806693},
-]
+    
+    function initMap() {
+        var map1 = new google.maps.Map(document.getElementById("maps-api-1"), {
+            zoom: 15,
+            center: {
+                lat: 53.4742028,
+                lng: -2.2447486
+            }
+        });
+
+        var map2 = new google.maps.Map(document.getElementById("maps-api-2"), {
+            zoom: 15,
+            center: {
+                lat: 53.809866,
+                lng: -1.5729377
+            }
+        });
+
+        var map3 = new google.maps.Map(document.getElementById("maps-api-3"), {
+            zoom: 15,
+            center: {
+                lat: 51.5235754,
+                lng: -0.0806693
+            }
+        });
+
+        var map4 = new google.maps.Map(document.getElementById("maps-api-4"), {
+            zoom: 15,
+            center: {
+                lat: 51.5235754,
+                lng: -0.0806693
+            }
+        });
+
+    }
+
+
