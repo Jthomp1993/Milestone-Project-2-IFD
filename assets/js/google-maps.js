a37645699061b4873b68d64a5eb3912ea54a0fe0
Jthomp1993 <joshuagt93@hotmail.com>
2020-10-29 16:51:58 +0000

    Added the logic to provide location of each venue with google maps api

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,37 +1,49 @@
-    
-    function initMap() {
-        var map1 = new google.maps.Map(document.getElementById("maps-api-1"), {
-            zoom: 15,
-            center: {
-                lat: 53.4742028,
-                lng: -2.2447486
-            }
-        });
 
-        var map2 = new google.maps.Map(document.getElementById("maps-api-2"), {
-            zoom: 15,
-            center: {
-                lat: 53.809866,
-                lng: -1.5729377
-            }
-        });
+/* Stephen from tutor assistance advised me to move my varibles outside of the function to prevent timed interference */
+const mylatLng1 = { lat: 53.4742028, lng: -2.2447486 };
+const mylatLng2 = { lat: 53.809866, lng: -1.5729377};
+const mylatLng3 = { lat: 51.5235754, lng: -0.0806693};
 
-        var map3 = new google.maps.Map(document.getElementById("maps-api-3"), {
-            zoom: 15,
-            center: {
-                lat: 51.5235754,
-                lng: -0.0806693
-            }
-        });
+function initMap() {
+  
+  var map = new google.maps.Map(document.getElementById("maps-api-1"), {
+    zoom: 15,
+    center: mylatLng1,
+  });
+  new google.maps.Marker({
+    position: mylatLng1,
+    map,
+    title: "A",
+  });
 
-        var map4 = new google.maps.Map(document.getElementById("maps-api-4"), {
-            zoom: 15,
-            center: {
-                lat: 51.5235754,
-                lng: -0.0806693
-            }
-        });
+  var map = new google.maps.Map(document.getElementById("maps-api-2"), {
+    zoom: 15,
+    center: mylatLng2,
+  });
+  new google.maps.Marker({
+    position: mylatLng2,
+    map,
+    title: "A",
+  });
 
-    }
+  var map = new google.maps.Map(document.getElementById("maps-api-3"), {
+    zoom: 15,
+    center: mylatLng3,
+  });
+  new google.maps.Marker({
+    position: mylatLng3,
+    map,
+    title: "A",
+  });
 
+  var map = new google.maps.Map(document.getElementById("maps-api-4"), {
+    zoom: 15,
+    center: mylatLng3,
+  });
+  new google.maps.Marker({
+    position: mylatLng3,
+    map,
+    title: "A",
+  });
+}
 
