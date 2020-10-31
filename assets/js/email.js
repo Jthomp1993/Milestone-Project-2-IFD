41f082ce02ce21527850c70671f6a836b228eede
Jthomp1993 <joshuagt93@hotmail.com>
2020-10-31 14:55:22 +0000

    EmailJS is now fully functioning for contact form in contact.html

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,15 +1,7 @@
-function sendMail(contactForm) {
-    emailjs.send("gmail", "tourist", {
-        "from_name": contactForm.name.value,
-        "from_email": contactForm.emailaddress.value,
-        "message": contactForm.message.value
-    })
-    .then(
-        function(response) {
-            console.log("SUCCESS", response);
-        },
-        function(error) {
-            console.log("FAILED", error);
-        });
-    return false;
+/* Kevin from tutor assistance advised me to use this new EmailJS method rather than the old method I using originally */
+window.onload = function () {
+    document.getElementById('contact-form').addEventListener('submit', function (event) {
+        event.preventDefault();
+        emailjs.sendForm('gmail', 'contact_form', this);
+    });
 }
