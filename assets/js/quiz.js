c7db83165d5ab3b93281e4c18d428bd203c52ed1
Jthomp1993 <joshuagt93@hotmail.com>
2020-11-07 14:53:19 +0000

    Added descriptive comments to quiz.js

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,7 +1,10 @@
+// This variable sets the score to 0
 var score = 0;
 
+// This function occurs when the DOM has been loaded
 $(document).ready(function() {
- 
+    
+    // This function changes the background color of the answers depending on if they are true or false
     $('.answers-btn').click(function() {
         if($(this).hasClass('true')) {
             $(this).addClass('btn-correct');
@@ -9,28 +12,32 @@ $(document).ready(function() {
         else { $(this).addClass('btn-incorrect');
     }
 });
-
+    
+    // This function changes the background color of the submit button
     $('.submit-btn').click(function() {
         if ($(this).hasClass('true')) {
             $(this).addClass('btn-correct');
         }
     });
-
+    
+    // If the answer is correct this function will increase the users score by 1
     $('.answers-btn').click(function() {
         if($(this).hasClass('true')) {
             score ++;
         }
     });
-
+    
+    // When the user clicks the try again button this function refreshes the page
     $('.refresh-button').click(function() {
         location.reload();
         location = location.href
     });
-
+    
+    // This function hides the results and try again button until the user clicks submit
     $('.submit-button').click(function() {
         $('.hide-element').show();
     });
-
+    // This function provides the user with there results and a comment on how they did
     $('.submit-button').click(function() {
         $('#total-score').text('You scored ' + score + ' out of 5')
         if (score === 5) {
